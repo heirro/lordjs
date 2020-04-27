47ba14a19b725747c9bbebcf8b77195aacb8b1ee
Vava Heirro <vavaheirro@live.com>
2020-04-28 05:04:34 +0700

    Add files via upload

---
+++ views/header.lord.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Single Page Application</title>
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+  </head>
+<h1>Single Page Application</h1>
+    <div>
+        <a onclick="window.close()" href="./#/home" target="_blank">Home</a>
+        <a onclick="window.close()" href="./#/user/user1" target="_blank">User1</a>
+        <a onclick="window.close()" href="./#/user/user2" target="_blank">User2</a>
+        <a onclick="window.close()" href="./#/about" target="_blank">About</a>
+        <a onclick="window.close()" href="./#/this-page-not-exist" target="_blank">404</a>
+    </div>
