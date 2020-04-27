19c29bd8c1093a5754f3ab6bf3c7fa1e7442a827
Vava Heirro <vavaheirro@live.com>
2020-04-28 05:22:14 +0700

    ganti awal

--- views/header.lord.php
+++ views/header.lord.php
@@ -7,9 +7,9 @@
   </head>
 <h1>Single Page Application</h1>
     <div>
-        <a onclick="window.close()" href="./#/home" target="_blank">Home</a>
-        <a onclick="window.close()" href="./#/user/user1" target="_blank">User1</a>
-        <a onclick="window.close()" href="./#/user/user2" target="_blank">User2</a>
-        <a onclick="window.close()" href="./#/about" target="_blank">About</a>
-        <a onclick="window.close()" href="./#/this-page-not-exist" target="_blank">404</a>
+        <a href="./?home">Home</a>
+        <a href="./?user/user1">User1</a>
+        <a href="./?user/user2">User2</a>
+        <a href="./?about">About</a>
+        <a href="./?this-page-not-exist">404</a>
     </div>
