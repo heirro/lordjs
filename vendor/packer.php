562f574e3e33fdf951d736cf1d48a952a900de81
Vava Heirro <vavaheirro@live.com>
2020-04-29 05:37:44 +0700

    Update packer.php

--- vendor/packer.php
+++ vendor/packer.php
@@ -2,4 +2,4 @@
 require __DIR__.'/autoload.php';
 $js = file_get_contents('./routing.js'); // don't change anything //
 $packer = new Tholu\Packer\Packer($js, 'Normal', true, false, true);
-$packed_js = $packer->pack();
+$routingjs = $packer->pack();
