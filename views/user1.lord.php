c56691e77dc85ad56b05c367cdecbff98590e731
Vava Heirro <vavaheirro@live.com>
2020-05-01 20:28:09 +0700

    Update user1.lord.php

--- views/user1.lord.php
+++ views/user1.lord.php
@@ -1,3 +1,10 @@
+<link rel="stylesheet" href="https://cdn.plyr.io/3.6.1/demo.css" />
+<style>
+.container {
+	margin: 20px auto;
+	max-width: 500px;
+}
+</style>
 <div class="container">
 <div id="player" data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>
 </div>
