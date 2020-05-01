31ad786cc35153016acf710e380c5a0ece65b5e5
Vava Heirro <vavaheirro@live.com>
2020-05-01 20:15:51 +0700

    Update user1.lord.php

--- views/user1.lord.php
+++ views/user1.lord.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="https://cdn.plyr.io/3.6.1/demo.css" />
 <div class="container">
 <div id="player" data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>
 </div>
-<script src="https://cdn.plyr.io/3.6.1/demo.js"></script>
